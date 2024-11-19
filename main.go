d39a91063d43b8ead1d3cec237a27f5d90ca04a2
AmarnathCJD <amarnathcharichilil@gmail.com>
2024-11-19 11:42:27 +0530

    umm

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
 	client, err := tg.NewClient(tg.ClientConfig{
 		AppID:    int32(appId),
 		AppHash:  os.Getenv("APP_HASH"),
-		LogLevel: tg.LogInfo,
+		LogLevel: tg.LogDebug,
 	})
 
 	if err != nil {
