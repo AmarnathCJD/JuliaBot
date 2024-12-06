5750fafc134d5891a95944c5d9877c0862839e01
AmarnathCJD <amarnathcharichilil@gmail.com>
2024-12-06 11:49:33 +0530

    umm

--- modules/spotify.go
+++ modules/spotify.go
@@ -199,17 +199,7 @@ func SpotifyInlineHandler(i *telegram.InlineQuery) error {
 		return nil
 	}
 
-	b.Document(telegram.InputMediaUploadedDocument{
-		File: fi,
-		Attributes: []telegram.DocumentAttribute{
-			&telegram.DocumentAttributeFilename{
-				FileName: "song.ogg",
-			},
-			&telegram.DocumentAttributeAudio{
-				Title:     response.Name,
-				Performer: response.Aritst,
-			},
-		},
+	b.Document(fi, &telegram.ArticleOptions{
 		MimeType: "audio/mpeg",
 	})
 	fmt.Println(i.Answer(b.Results()))
