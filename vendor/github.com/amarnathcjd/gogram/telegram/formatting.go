77de4b77a4972321c27337e3f3564f05a4110c8c
AmarnathCJD <amarnathcharichilil@gmail.com>
2024-12-25 21:33:31 +0530

    mhm temp aytt

---
+++ vendor/github.com/amarnathcjd/gogram/telegram/formatting.go
@@ -0,0 +1,401 @@
+// Copyright (c) 2024 RoseLoverX
+
+package telegram
+
+import (
+	"bytes"
+	"strconv"
+
+	"strings"
+	"unicode/utf16"
+
+	"golang.org/x/net/html"
+)
+
+func (c *Client) FormatMessage(message, mode string) ([]MessageEntity, string) {
+	return parseEntities(message, mode)
+}
+
+// parseEntities parses the message and returns a list of MessageEntities and the cleaned text string
+func parseEntities(message, mode string) ([]MessageEntity, string) {
+	if strings.EqualFold(mode, HTML) {
+		return parseHTML(message)
+	} else if strings.EqualFold(mode, MarkDown) {
+		return parseMarkdown(message)
+	}
+	return []MessageEntity{}, message
+}
+
+// parseHTML parses HTML and returns a list of MessageEntities and the cleaned text string
+func parseHTML(text string) ([]MessageEntity, string) {
+	cleanedText, tags, err := parseHTMLToTags(text)
+	if err != nil {
+		return []MessageEntity{}, text
+	}
+
+	entities := parseTagsToEntity(tags)
+	return entities, cleanedText
+}
+
+// parseMarkdown parses Markdown and returns a list of MessageEntities and the cleaned text string
+func parseMarkdown(text string) ([]MessageEntity, string) {
+	htmlStr := HTMLToMarkdownV2(text)
+	return parseHTML(htmlStr)
+}
+
+// Tag represents a tag in the HTML string, including its type, length, and offset and whether it has nested tags, and its attrs
+type Tag struct {
+	Type      string `json:"type"`
+	Length    int32  `json:"length"`
+	Offset    int32  `json:"offset"`
+	hasNested bool
+	Attrs     map[string]string
+}
+
+// supportedTag returns true if the tag is supported by the parser
+func supportedTag(tag string) bool {
+	switch tag {
+	case "b", "strong", "i", "em", "u", "s", "a", "code", "pre", "ins", "del", "spoiler", "quote", "blockquote", "emoji", "mention":
+		return true
+	}
+	return false
+}
+
+func parseHTMLToTags(htmlStr string) (string, []Tag, error) {
+	// Parse the HTML string into a tree of nodes
+	doc, err := html.Parse(strings.NewReader(htmlStr))
+	if err != nil {
+		return "", nil, err
+	}
+
+	// Convert the tree of nodes into a string with no HTML tags
+	var textBuf bytes.Buffer
+	tagOffsets := []Tag{}
+	var parseNode func(*html.Node, int32)
+	var openTags []Tag
+	parseNode = func(n *html.Node, offset int32) {
+		if n.Type == html.ElementNode {
+			// Only record tag information for non-body, non-html, non-head, non-p tags
+			if supportedTag(n.Data) {
+				tagType := n.Data
+				tagLength := getTextLength(n)
+				TagAttrs := make(map[string]string)
+				for _, attr := range n.Attr {
+					TagAttrs[attr.Key] = attr.Val
+				}
+
+				tagOffset := utf16RuneCountInString(textBuf.String())
+				tagOffsets = append(tagOffsets, Tag{Type: tagType, Length: tagLength, Offset: tagOffset, Attrs: TagAttrs})
+
+				// if tag not closed, add to open tags
+				if n.FirstChild != nil && n.FirstChild.NextSibling == nil {
+					openTags = append(openTags, Tag{Type: tagType, Length: tagLength, Offset: tagOffset})
+				}
+
+			}
+		} else if n.Type == html.TextNode {
+			// Write the text content of this node to the buffer
+			textBuf.WriteString(n.Data)
+			offset += utf16RuneCountInString(n.Data)
+		}
+
+		// Recursively process child nodes
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			parseNode(c, offset)
+		}
+
+		// Check if any open tags are closed by this node
+		for i := len(openTags) - 1; i >= 0; i-- {
+			if openTags[i].Type == n.Data {
+				openTags[i].Length = utf16RuneCountInString(textBuf.String()) - openTags[i].Offset
+				openTags[i].hasNested = true
+				openTags = openTags[:i]
+			}
+		}
+	}
+
+	parseNode(doc, 0)
+
+	// Adjust the length of any unclosed tags at the end of the string
+	lastOffset := utf16RuneCountInString(textBuf.String())
+	for i := range openTags {
+		openTags[i].Length = lastOffset - openTags[i].Offset
+	}
+
+	// Return the cleaned text string and tag offsets list
+	cleanedText := strings.TrimSpace(textBuf.String())
+	return cleanedText, tagOffsets, nil
+}
+
+func trimTrailing(input string) string {
+	lastNewlineIndex := strings.LastIndex(input, "\n")
+	if lastNewlineIndex != -1 && strings.TrimSpace(input[lastNewlineIndex:]) == "" {
+		return input[:lastNewlineIndex]
+	}
+
+	return input
+}
+
+// getTextLength returns the length of the text content of a node, including its children
+func getTextLength(n *html.Node) int32 {
+	var tagLength int32 = 0
+	currentNode := n.FirstChild
+	for currentNode != nil {
+		if currentNode.Type == html.TextNode {
+			tagLength += utf16RuneCountInString(trimTrailing(currentNode.Data))
+		} else if currentNode.Type == html.ElementNode {
+			tagLength += getTextLength(currentNode)
+		}
+		currentNode = currentNode.NextSibling
+	}
+	return tagLength
+}
+
+// utf16RuneCountInString returns the number of UTF-16 code units in a string
+func utf16RuneCountInString(s string) int32 {
+	return int32(len(utf16.Encode([]rune(s))))
+}
+
+// parseTagsToEntity converts a list of tags to a list of MessageEntities
+func parseTagsToEntity(tags []Tag) []MessageEntity {
+	var entities []MessageEntity
+	for _, tag := range tags {
+		switch tag.Type {
+		case "a":
+			switch {
+			case tag.Attrs["href"] != "" && strings.HasPrefix(tag.Attrs["href"], "mailto:"):
+				entities = append(entities, &MessageEntityEmail{tag.Offset, tag.Length})
+
+			case tag.Attrs["href"] == "":
+				entities = append(entities, &MessageEntityURL{tag.Offset, tag.Length})
+			default:
+				entities = append(entities, &MessageEntityTextURL{tag.Offset, tag.Length, tag.Attrs["href"]})
+			}
+		case "b", "strong":
+			entities = append(entities, &MessageEntityBold{tag.Offset, tag.Length})
+		case "code":
+			entities = append(entities, &MessageEntityCode{tag.Offset, tag.Length})
+		case "em", "i":
+			entities = append(entities, &MessageEntityItalic{tag.Offset, tag.Length})
+		case "pre":
+			entities = append(entities, &MessageEntityPre{tag.Offset, tag.Length, tag.Attrs["language"]})
+		case "s", "strike", "del":
+			entities = append(entities, &MessageEntityStrike{tag.Offset, tag.Length})
+		case "u":
+			entities = append(entities, &MessageEntityUnderline{tag.Offset, tag.Length})
+		case "mention":
+			entities = append(entities, &MessageEntityMention{tag.Offset, tag.Length})
+		case "spoiler":
+			entities = append(entities, &MessageEntitySpoiler{tag.Offset, tag.Length})
+		case "quote", "blockquote":
+			isCollapsed := false
+			if parsed, err := strconv.ParseBool(tag.Attrs["collapsed"]); err == nil {
+				isCollapsed = parsed
+			}
+			entities = append(entities, &MessageEntityBlockquote{isCollapsed, tag.Offset, tag.Length})
+		case "emoji":
+			emoijiId, err := strconv.ParseInt(tag.Attrs["id"], 10, 64)
+			if err != nil {
+				continue
+			}
+			entities = append(entities, &MessageEntityCustomEmoji{tag.Offset, tag.Length, emoijiId})
+		}
+	}
+	return entities
+}
+
+func HTMLToMarkdownV2(markdown string) string {
+	markdown, placeholders := handleEscapes(markdown)
+	markdown = convertSyntax(markdown, "**", "<b>", "</b>")
+	markdown = convertSyntax(markdown, "_", "<i>", "</i>")
+	markdown = convertSyntax(markdown, "*", "<i>", "</i>")
+	markdown = convertSyntax(markdown, "!!", "<u>", "</u>")
+	markdown = convertSyntax(markdown, "__", "<u>", "</u>")
+	markdown = convertSyntax(markdown, "~", "<s>", "</s>")
+	markdown = convertSyntax(markdown, "||", "<spoiler>", "</spoiler>")
+	markdown = convertCodeSyntax(markdown)
+	markdown = convertCodeBlockSyntax(markdown)
+	markdown = convertLinksSyntax(markdown)
+	markdown = convertEmojiSyntax(markdown)
+	markdown = convertBlockquotesSyntax(markdown)
+	markdown = restoreEscapes(markdown, placeholders)
+	return string(bytes.TrimSpace([]byte(markdown)))
+}
+
+func handleEscapes(markdown string) (string, map[string]string) {
+	escapes := []string{"\\*", "\\_", "\\~", "\\|", "\\`", "\\[", "\\]", "\\(", "\\)", "\\{", "\\}", "\\<", "\\>", "\\!"}
+	placeholders := make(map[string]string)
+
+	for i, esc := range escapes {
+		placeholder := "??ESC??" + strconv.Itoa(i) + "??"
+		placeholders[placeholder] = escapes[i]
+		markdown = strings.ReplaceAll(markdown, esc, placeholder)
+	}
+
+	return markdown, placeholders
+}
+
+func restoreEscapes(markdown string, placeholders map[string]string) string {
+	for placeholder, esc := range placeholders {
+		markdown = strings.ReplaceAll(markdown, placeholder, strings.ReplaceAll(esc, "\\", ""))
+	}
+	return markdown
+}
+
+func convertSyntax(markdown, delimiter, openTag, closeTag string) string {
+	for {
+		start := strings.Index(markdown, delimiter)
+		if start == -1 {
+			break
+		}
+		end := strings.Index(markdown[start+len(delimiter):], delimiter)
+		if end == -1 {
+			break
+		}
+		end += start + len(delimiter)
+		markdown = markdown[:start] + openTag + markdown[start+len(delimiter):end] + closeTag + markdown[end+len(delimiter):]
+	}
+	return markdown
+}
+
+func convertCodeSyntax(markdown string) string {
+	for {
+		start := strings.Index(markdown, "`")
+		if start == -1 {
+			break
+		}
+		end := strings.Index(markdown[start+1:], "`")
+		if end == -1 {
+			break
+		}
+		end += start + 1
+		content := html.EscapeString(markdown[start+1 : end])
+		markdown = markdown[:start] + "<code>" + content + "</code>" + markdown[end+1:]
+	}
+	return markdown
+}
+
+func convertCodeBlockSyntax(markdown string) string {
+	for {
+		start := strings.Index(markdown, "```")
+		if start == -1 {
+			break
+		}
+		end := strings.Index(markdown[start+3:], "```")
+		if end == -1 {
+			break
+		}
+		end += start + 3
+		// Extract the language if specified
+		codeBlock := markdown[start+3 : end]
+		var lang string
+		if strings.Contains(codeBlock, "\n") {
+			langEnd := strings.Index(codeBlock, "\n")
+			lang = codeBlock[:langEnd]
+			codeBlock = codeBlock[langEnd+1:]
+		}
+		content := html.EscapeString(codeBlock)
+		if lang != "" {
+			markdown = markdown[:start] + "<pre><code class=\"language-" + html.EscapeString(lang) + "\">" + content + "</code></pre>" + markdown[end+3:]
+		} else {
+			markdown = markdown[:start] + "<pre><code>" + content + "</code></pre>" + markdown[end+3:]
+		}
+	}
+	return markdown
+}
+
+func convertLinksSyntax(markdown string) string {
+	for {
+		start := strings.Index(markdown, "[")
+		if start == -1 {
+			break
+		}
+		middle := strings.Index(markdown[start:], "](")
+		if middle == -1 {
+			break
+		}
+		middle += start
+		end := strings.Index(markdown[middle:], ")")
+		if end == -1 {
+			break
+		}
+		end += middle
+		text := markdown[start+1 : middle]
+		url := html.EscapeString(markdown[middle+2 : end])
+		markdown = markdown[:start] + "<a href=\"" + url + "\">" + text + "</a>" + markdown[end+1:]
+	}
+	return markdown
+}
+
+func convertEmojiSyntax(markdown string) string {
+	for {
+		start := strings.Index(markdown, "::")
+		if start == -1 {
+			break
+		}
+		end := strings.Index(markdown[start+2:], "::")
+		if end == -1 {
+			break
+		}
+		end += start + 2
+		markdown = markdown[:start] + "<emoji id=\"" + markdown[start+2:end] + "\">" + "</emoji>" + markdown[end+2:]
+	}
+	return markdown
+}
+
+func convertBlockquotesSyntax(markdown string) string {
+	var result strings.Builder
+	lines := strings.Split(markdown, "\n")
+	inBlockquote := false
+	collapsedBlockquote := false
+
+	for i, line := range lines {
+		trimmedLine := strings.TrimSpace(line)
+		if inBlockquote && !strings.HasPrefix(trimmedLine, "> ") && !strings.HasPrefix(trimmedLine, ">> ") {
+			result.WriteString("</blockquote>\n")
+			inBlockquote = false
+			collapsedBlockquote = false
+		}
+
+		if strings.HasPrefix(trimmedLine, "> ") {
+			if !inBlockquote || collapsedBlockquote {
+				if i > 0 {
+					result.WriteString("")
+				}
+				if collapsedBlockquote {
+					result.WriteString("</blockquote>\n")
+					collapsedBlockquote = false
+				}
+				result.WriteString("<blockquote>")
+				inBlockquote = true
+			}
+			result.WriteString(strings.TrimPrefix(trimmedLine, "> ") + "\n")
+		} else if strings.HasPrefix(trimmedLine, ">> ") {
+			if !inBlockquote || !collapsedBlockquote {
+				if i > 0 {
+					result.WriteString("")
+				}
+				if inBlockquote && !collapsedBlockquote {
+					result.WriteString("</blockquote>\n")
+				}
+				result.WriteString("<blockquote collapsed=\"true\">")
+				inBlockquote = true
+				collapsedBlockquote = true
+			}
+			result.WriteString(strings.TrimPrefix(trimmedLine, ">> ") + "\n")
+		} else {
+			if inBlockquote {
+				result.WriteString("</blockquote>\n")
+				inBlockquote = false
+				collapsedBlockquote = false
+			}
+			result.WriteString(line + "\n")
+		}
+	}
+
+	if inBlockquote {
+		result.WriteString("</blockquote>\n")
+	}
+
+	return result.String()
+}
---
+++ vendor/github.com/amarnathcjd/gogram/telegram/users.go
@@ -0,0 +1,412 @@
+package telegram
+
+import (
+	"reflect"
+	"time"
+
+	"github.com/pkg/errors"
+)
+
+// GetMe returns the current user
+func (c *Client) GetMe() (*UserObj, error) {
+	resp, err := c.UsersGetFullUser(&InputUserSelf{})
+	if err != nil {
+		return nil, errors.Wrap(err, "getting user")
+	}
+	user, ok := resp.Users[0].(*UserObj)
+	if !ok {
+		return nil, errors.New("got wrong response: " + reflect.TypeOf(resp).String())
+	}
+	c.clientData.me = user
+
+	return user, nil
+}
+
+type PhotosOptions struct {
+	MaxID  int64 `json:"max_id,omitempty"`
+	Offset int32 `json:"offset,omitempty"`
+	Limit  int32 `json:"limit,omitempty"`
+}
+
+type UserPhoto struct {
+	Photo Photo
+}
+
+func (p *UserPhoto) FileID() string {
+	return PackBotFileID(p.Photo)
+}
+
+func (p *UserPhoto) FileSize() int64 {
+	if p, ok := p.Photo.(*PhotoObj); ok {
+		if p.VideoSizes != nil {
+			return int64(p.VideoSizes[len(p.VideoSizes)-1].(*VideoSizeObj).Size)
+		}
+		size, _ := getPhotoSize(p.Sizes[len(p.Sizes)-1])
+		return size
+	}
+	return 0
+}
+
+func (p *UserPhoto) DcID() int32 {
+	if p, ok := p.Photo.(*PhotoObj); ok {
+		return p.DcID
+	}
+	return 4
+}
+
+func (p *UserPhoto) InputLocation() (*InputPhotoFileLocation, error) {
+	if photo, ok := p.Photo.(*PhotoObj); ok {
+		if photo.VideoSizes != nil {
+			return &InputPhotoFileLocation{
+				ID:            photo.ID,
+				AccessHash:    photo.AccessHash,
+				FileReference: photo.FileReference,
+				ThumbSize:     photo.VideoSizes[0].(*VideoSizeObj).Type,
+			}, nil
+		}
+		_, thumbSize := getPhotoSize(photo.Sizes[len(photo.Sizes)-1])
+		return &InputPhotoFileLocation{
+			ID:            photo.ID,
+			AccessHash:    photo.AccessHash,
+			FileReference: photo.FileReference,
+			ThumbSize:     thumbSize,
+		}, nil
+	}
+	return nil, errors.New("could not convert photo: " + reflect.TypeOf(p.Photo).String())
+}
+
+// GetProfilePhotos returns the profile photos of a user
+//
+//	Params:
+//	 - userID: The user ID
+//	 - Offset: The offset to start from
+//	 - Limit: The number of photos to return
+//	 - MaxID: The maximum ID of the photo to return
+func (c *Client) GetProfilePhotos(userID any, Opts ...*PhotosOptions) ([]UserPhoto, error) {
+	Options := getVariadic(Opts, &PhotosOptions{})
+	if Options.Limit > 80 {
+		Options.Limit = 80
+	} else if Options.Limit < 1 {
+		Options.Limit = 1
+	}
+	peer, err := c.ResolvePeer(userID)
+	if err != nil {
+		return nil, err
+	}
+	User, ok := peer.(*InputPeerUser)
+	if !ok {
+		return nil, errors.New("given peer is not a user")
+	}
+	resp, err := c.PhotosGetUserPhotos(
+		&InputUserObj{UserID: User.UserID, AccessHash: User.AccessHash},
+		Options.Offset,
+		Options.MaxID,
+		Options.Limit,
+	)
+	if err != nil {
+		return nil, err
+	}
+	switch p := resp.(type) {
+	case *PhotosPhotosObj:
+		c.Cache.UpdatePeersToCache(p.Users, []Chat{})
+		photos := make([]UserPhoto, len(p.Photos))
+		for i, photo := range p.Photos {
+			photos[i] = UserPhoto{Photo: photo}
+		}
+		return photos, nil
+	case *PhotosPhotosSlice:
+		c.Cache.UpdatePeersToCache(p.Users, []Chat{})
+		photos := make([]UserPhoto, len(p.Photos))
+		for i, photo := range p.Photos {
+			photos[i] = UserPhoto{Photo: photo}
+		}
+		return photos, nil
+	default:
+		return nil, errors.New("could not convert photos: " + reflect.TypeOf(resp).String())
+	}
+}
+
+type DialogOptions struct {
+	OffsetID         int32     `json:"offset_id,omitempty"`
+	OffsetDate       int32     `json:"offset_date,omitempty"`
+	OffsetPeer       InputPeer `json:"offset_peer,omitempty"`
+	Limit            int32     `json:"limit,omitempty"`
+	ExcludePinned    bool      `json:"exclude_pinned,omitempty"`
+	FolderID         int32     `json:"folder_id,omitempty"`
+	Hash             int64     `json:"hash,omitempty"`
+	SleepThresholdMs int32     `json:"sleep_threshold_ms,omitempty"`
+}
+
+type CustomDialog struct {
+	Dialog   Dialog
+	Peer     Peer
+	PeerType string
+} // TODO
+
+// GetDialogs returns the dialogs of the user
+//
+//	Params:
+//	 - OffsetID: The offset ID of the dialog
+//	 - OffsetDate: The offset date of the dialog
+//	 - OffsetPeer: The offset peer of the dialog
+//	 - Limit: The number of dialogs to return
+//	 - ExcludePinned: Whether to exclude pinned dialogs
+//	 - FolderID: The folder ID to get dialogs from
+//	 - Hash: The hash of the dialogs
+//	 - SleepThresholdMs: The sleep threshold in milliseconds, to avoid flooding
+func (c *Client) GetDialogs(Opts ...*DialogOptions) ([]Dialog, error) {
+	options := getVariadic(Opts, &DialogOptions{
+		Limit:            1,
+		OffsetPeer:       &InputPeerEmpty{},
+		SleepThresholdMs: 20,
+	})
+	if options.OffsetPeer == nil {
+		options.OffsetPeer = &InputPeerEmpty{}
+	}
+
+	if options.SleepThresholdMs == 0 {
+		options.SleepThresholdMs = 20
+	}
+
+	var req = &MessagesGetDialogsParams{
+		OffsetDate:    options.OffsetDate,
+		OffsetID:      options.OffsetID,
+		OffsetPeer:    options.OffsetPeer,
+		ExcludePinned: options.ExcludePinned,
+		FolderID:      options.FolderID,
+		Hash:          options.Hash,
+	}
+
+	var dialogs []Dialog
+	var fetched int
+
+	for {
+		remaining := options.Limit - int32(fetched)
+		perReqLimit := int32(100)
+		if remaining < perReqLimit {
+			perReqLimit = remaining
+		}
+
+		req.Limit = perReqLimit
+
+		resp, err := c.MessagesGetDialogs(req)
+		if handleIfFlood(err, c) {
+			continue
+		} else if err != nil {
+			return nil, err
+		}
+
+		switch p := resp.(type) {
+		case *MessagesDialogsObj:
+			c.Cache.UpdatePeersToCache(p.Users, p.Chats)
+			dialogs = append(dialogs, p.Dialogs...)
+			if len(p.Messages) > 0 {
+				if m, ok := p.Messages[len(p.Messages)-1].(*MessageObj); ok {
+					req.OffsetID = m.ID
+					req.OffsetDate = m.Date
+				}
+			}
+
+			if lastPeer, err := c.GetSendablePeer(p.Dialogs[len(p.Dialogs)-1].(*DialogObj).Peer); err == nil {
+				req.OffsetPeer = lastPeer
+			}
+
+			fetched += len(p.Dialogs)
+			if len(p.Dialogs) < int(perReqLimit) || fetched >= int(options.Limit) && options.Limit > 0 {
+				return dialogs, nil
+			}
+
+		case *MessagesDialogsSlice:
+			c.Cache.UpdatePeersToCache(p.Users, p.Chats)
+			dialogs = append(dialogs, p.Dialogs...)
+
+			if len(p.Messages) > 0 {
+				if m, ok := p.Messages[len(p.Messages)-1].(*MessageObj); ok {
+					req.OffsetID = m.ID
+					req.OffsetDate = m.Date
+				}
+			}
+
+			if lastPeer, err := c.GetSendablePeer(p.Dialogs[len(p.Dialogs)-1].(*DialogObj).Peer); err == nil {
+				req.OffsetPeer = lastPeer
+			}
+
+			fetched += len(p.Dialogs)
+			if len(p.Dialogs) < int(perReqLimit) || fetched >= int(options.Limit) && options.Limit > 0 {
+				return dialogs, nil
+			}
+
+		case *MessagesDialogsNotModified:
+			return dialogs, nil
+
+		default:
+			return nil, errors.New("could not convert dialogs: " + reflect.TypeOf(resp).String())
+		}
+
+		time.Sleep(time.Duration(options.SleepThresholdMs) * time.Millisecond)
+	}
+}
+
+func (c *Client) IterDialogs(Opts ...*DialogOptions) (<-chan Dialog, <-chan bool, <-chan error) {
+	options := getVariadic(Opts, &DialogOptions{
+		Limit:            1,
+		OffsetPeer:       &InputPeerEmpty{},
+		SleepThresholdMs: 20,
+	})
+
+	if options.OffsetPeer == nil {
+		options.OffsetPeer = &InputPeerEmpty{}
+	}
+
+	if options.SleepThresholdMs == 0 {
+		options.SleepThresholdMs = 20
+	}
+
+	var req = &MessagesGetDialogsParams{
+		OffsetDate:    options.OffsetDate,
+		OffsetID:      options.OffsetID,
+		OffsetPeer:    options.OffsetPeer,
+		ExcludePinned: options.ExcludePinned,
+		FolderID:      options.FolderID,
+		Hash:          options.Hash,
+	}
+
+	dialogs := make(chan Dialog)
+	done := make(chan bool)
+	errs := make(chan error)
+
+	go func() {
+		defer close(dialogs)
+		defer close(done)
+		defer close(errs)
+
+		var fetched int
+
+		for {
+			remaining := options.Limit - int32(fetched)
+			perReqLimit := int32(100)
+			if remaining < perReqLimit {
+				perReqLimit = remaining
+			}
+
+			req.Limit = perReqLimit
+
+			resp, err := c.MessagesGetDialogs(req)
+			if handleIfFlood(err, c) {
+				continue
+			} else if err != nil {
+				errs <- err
+				return
+			}
+
+			switch p := resp.(type) {
+			case *MessagesDialogsObj:
+				c.Cache.UpdatePeersToCache(p.Users, p.Chats)
+				for _, dialog := range p.Dialogs {
+					dialogs <- dialog
+				}
+
+				if len(p.Messages) > 0 {
+					if m, ok := p.Messages[len(p.Messages)-1].(*MessageObj); ok {
+						req.OffsetID = m.ID
+						req.OffsetDate = m.Date
+					}
+				}
+
+				if lastPeer, err := c.GetSendablePeer(p.Dialogs[len(p.Dialogs)-1].(*DialogObj).Peer); err == nil {
+					req.OffsetPeer = lastPeer
+				}
+
+				fetched += len(p.Dialogs)
+				if len(p.Dialogs) < int(perReqLimit) || fetched >= int(options.Limit) && options.Limit > 0 {
+					done <- true
+					return
+				}
+
+			case *MessagesDialogsSlice:
+				c.Cache.UpdatePeersToCache(p.Users, p.Chats)
+				for _, dialog := range p.Dialogs {
+					dialogs <- dialog
+				}
+
+				if len(p.Messages) > 0 {
+					if m, ok := p.Messages[len(p.Messages)-1].(*MessageObj); ok {
+						req.OffsetID = m.ID
+						req.OffsetDate = m.Date
+					}
+				}
+
+				if lastPeer, err := c.GetSendablePeer(p.Dialogs[len(p.Dialogs)-1].(*DialogObj).Peer); err == nil {
+					req.OffsetPeer = lastPeer
+				}
+
+				fetched += len(p.Dialogs)
+				if len(p.Dialogs) < int(perReqLimit) || fetched >= int(options.Limit) && options.Limit > 0 {
+					done <- true
+					return
+				}
+
+			case *MessagesDialogsNotModified:
+				done <- true
+				return
+
+			default:
+				errs <- errors.New("could not convert dialogs: " + reflect.TypeOf(resp).String())
+				return
+			}
+
+			time.Sleep(time.Duration(options.SleepThresholdMs) * time.Millisecond)
+		}
+	}()
+
+	return dialogs, done, errs
+}
+
+// GetCommonChats returns the common chats of a user
+//
+//	Params:
+//	 - userID: The user Identifier
+func (c *Client) GetCommonChats(userID any) ([]Chat, error) {
+	peer, err := c.ResolvePeer(userID)
+	if err != nil {
+		return nil, err
+	}
+	user, ok := peer.(*InputPeerUser)
+	if !ok {
+		return nil, errors.New("peer is not a user")
+	}
+	resp, err := c.MessagesGetCommonChats(&InputUserObj{UserID: user.UserID, AccessHash: user.AccessHash}, 0, 100)
+	if err != nil {
+		return nil, err
+	}
+	switch p := resp.(type) {
+	case *MessagesChatsObj:
+		c.Cache.UpdatePeersToCache([]User{}, p.Chats)
+		return p.Chats, nil
+	case *MessagesChatsSlice:
+		c.Cache.UpdatePeersToCache([]User{}, p.Chats)
+		return p.Chats, nil
+	default:
+		return nil, errors.New("could not convert chats: " + reflect.TypeOf(resp).String())
+	}
+}
+
+// SetEmojiStatus sets the emoji status of the user
+//
+//	Params:
+//	 - emoji: The emoji status to set
+func (c *Client) SetEmojiStatus(emoji ...any) (bool, error) {
+	var status EmojiStatus
+	if len(emoji) == 0 {
+		status = &EmojiStatusEmpty{}
+	} else {
+		em := emoji[0]
+		_, ok := em.(EmojiStatus)
+		if !ok {
+			return false, errors.New("emoji is not an EmojiStatus")
+		}
+		status = em.(EmojiStatus)
+	}
+	_, err := c.AccountUpdateEmojiStatus(status)
+	return err == nil, err
+}
