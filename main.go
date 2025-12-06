afbb93098dce3f1b38484754db45cfdbfe192a90
Amarnath C <camarnath714@gmail.com>
2025-12-06 23:51:04 +0530

    Update main.go

--- main.go
+++ main.go
@@ -59,7 +59,7 @@ func main() {
 	// })
 	client.OnCommand("senders", func(m *tg.NewMessage) error {
 		x := client.GetExportedSendersStatus()
-		m.Reply(fmt.Sprintf(x))
+		m.Reply(fmt.Sprintf("%+v", x))
 		return nil
 	})
 	client.Idle()
